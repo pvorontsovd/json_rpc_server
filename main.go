8ad252ecac2cc086618950eb6a35b6104c254df2
Pavel Vorontsov <pvorontsovd@gmail.com>
2020-02-08 21:53:23 +0300

    add flags

--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"json_rpc_server/api"
 	"json_rpc_server/db"
 	"log"
 )
 
 func main() {
-	p := db.New(
-		"postgres",
-		"127.0.0.1",
-		"pgadmin",
-		"pgadmin",
-		54321,
-	)
+	dbName := flag.String("db.name", "postgres", "database name")
+	dbHost := flag.String("db.host", "127.0.0.1", "database host")
+	dbPort := flag.Int("db.port", 5432, "databse port")
+	dbUser := flag.String("db.user", "pgadmin", "database user")
+	dbPass := flag.String("db.pass", "pgadmin", "database password")
+	apiHost := flag.String("api.host", "127.0.0.1", "api host")
+	apiPort := flag.Int("api.port", 8080, "api port")
+	flag.Parse()
 
-	if err := p.Open(); err != nil {
+	db := db.New(*dbName, *dbHost, *dbUser, *dbPass, *dbPort)
+
+	if err := db.Open(); err != nil {
 		log.Fatal(err)
 	}
-	defer p.Close()
+	defer db.Close()
 
-	if err := p.Init(); err != nil {
+	if err := db.Init(); err != nil {
 		log.Fatal(err)
 	}
 
-	api := api.New("127.0.0.1:8080", p)
+	api := api.New(fmt.Sprintf("%s:%d", *apiHost, *apiPort), db)
 
 	if err := api.Listen(); err != nil {
 		log.Fatal(err)
